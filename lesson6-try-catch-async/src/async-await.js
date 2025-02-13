cf0deffc9a1537013323f63cd396d793a214134b
Bohdan_Yankevych <777janek777@gmail.com>
2025-02-13 23:33:33 +0100

    added changes

--- lesson6-try-catch-async/src/async-await.js
+++ lesson6-try-catch-async/src/async-await.js
@@ -24,4 +24,7 @@ function processJsonDataAsync(data) {
 }
 
 
-fetchDataWithAsyncAwait(apiUrlAsync);
+//fetchDataWithAsyncAwait(apiUrlAsync);
+(async () => {
+    await fetchDataWithAsyncAwait(apiUrlAsync);
+})();
