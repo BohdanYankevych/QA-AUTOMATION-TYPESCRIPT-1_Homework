d5ad2dcee02e2bb1bbc565f433863a1bd7eb8a93
Bohdan_Yankevych <777janek777@gmail.com>
2025-02-18 17:02:13 +0100

    delited folder

---
+++ lesson8 interfaces-and-types-classes/src/types.ts
@@ -0,0 +1,22 @@
+export type Person = {
+    firstName: string;
+    lastName: string;
+    yearsOfBirdth: number;
+};
+const person: Partial<Person> = {};
+
+person.firstName = 'John';
+person.lastName = 'Doe';
+person.yearsOfBirdth = 1990;
+
+console.log(person);
+
+type State = 'loaded' | 'loading' | 'error';
+let state: State;
+state = 'loading';
+state = 'loaded';
+
+const state2 = 'error' as State;
+
+console.log(state, state2);
+
