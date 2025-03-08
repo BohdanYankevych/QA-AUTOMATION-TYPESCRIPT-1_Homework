c59524249b95de642bc2cf067cec9e5a0926160f
Bohdan_Yankevych <777janek777@gmail.com>
2025-03-08 21:23:33 +0100

    deleted commented files

--- lesson11-mock-in-unit-testing/mocha-sinon/tests/electric-water-heater.spec.ts
+++ lesson11-mock-in-unit-testing/mocha-sinon/tests/electric-water-heater.spec.ts
@@ -1,28 +1,28 @@
-// import sinon from 'ts-sinon';
-// import { ElectricWaterHeater } from '../src/electric-water-heater';
-// import { expect } from 'chai';
+import sinon from 'ts-sinon';
+import { ElectricWaterHeater } from '../src/electric-water-heater';
+import { expect } from 'chai';
 
-// describe('ElectricWaterHeater stubbing tests', () => {
-//     describe('ElectricWaterHeater with object instance', () => {
-//         const heater = new ElectricWaterHeater(50, '25x25x50');
+describe('ElectricWaterHeater stubbing tests', () => {
+    describe('ElectricWaterHeater with object instance', () => {
+        const heater = new ElectricWaterHeater(50, '25x25x50');
 
-//         const instance = sinon.stub(heater);
+        const instance = sinon.stub(heater);
 
-//         it('heatWater should heat the water to 50 degrees', () => {
-//             instance.heatWater();
-//             expect(instance.currentTemperature).to.be.equal(50);
-//         });
+        it('heatWater should heat the water to 50 degrees', () => {
+            instance.heatWater();
+            expect(instance.currentTemperature).to.be.equal(50);
+        });
 
-//     });
+    });
 
-//     describe('ElectricWaterHeater with object stab', () => {
-//         const instance = sinon.createStubInstance(ElectricWaterHeater);
+    describe('ElectricWaterHeater with object stab', () => {
+        const instance = sinon.createStubInstance(ElectricWaterHeater);
 
-//         it('heatWater should heat throw the error', () => {
-//             instance.getDimensions.returns('5x25x50');
-//             expect(instance.getDimensions()).to.be.equal('5x25x50');
-//             expect(instance.getDimensions.calledOnce).to.be.true;
-//         });
+        it('heatWater should heat throw the error', () => {
+            instance.getDimensions.returns('5x25x50');
+            expect(instance.getDimensions()).to.be.equal('5x25x50');
+            expect(instance.getDimensions.calledOnce).to.be.true;
+        });
 
-//     });
-// });
+    });
+});
